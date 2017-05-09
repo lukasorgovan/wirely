c82d777095b8bdf83c39a13e654f28dbf74815ea
Lukas Orgovan <lukas.orgovan@gmail.com>
2017-05-09 11:10:12 +0200

    debugbar added

--- config/app.php
+++ config/app.php
@@ -155,6 +155,10 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        /*
+         * Debug debugbar service provider...
+         */
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
